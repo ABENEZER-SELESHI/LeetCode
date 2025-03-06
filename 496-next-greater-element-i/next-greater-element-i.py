ce87d0c392ed084384d00400cec21736808543fc
ABENEZER-SELESHI <145642956+ABENEZER-SELESHI@users.noreply.github.com>
2025-03-06 07:00:43 -0800

    Time: 1 ms (53.82%) | Memory: 18 MB (40.58%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,18 +1,34 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        res = []
+        res = [-1]*len(nums1)
+        mydict = defaultdict(int)
+        stack = deque()
+
+        for i in range(len(nums2)):
+            while stack and stack[-1] < nums2[i]:
+                mydict[stack[-1]] = nums2[i]
+                stack.pop()
+            stack.append(nums2[i])
+        
         for i in range(len(nums1)):
-            mn = float("inf")
-            n = len(nums2) - 1
-            if nums2[n] == nums1[i]:
-                res.append(-1)
-                continue
-            while nums2[n] != nums1[i]:
-                if nums2[n] > nums1[i]:
-                    mn = nums2[n]
-                n -= 1
-            if mn == float("inf"):
-                res.append(-1)
-            else:
-                res.append(mn)
+            if nums1[i] in mydict:
+                res[i] = mydict[nums1[i]]
+        
         return res
+        
+
+        # for i in range(len(nums1)):
+        #     mn = float("inf")
+        #     n = len(nums2) - 1
+        #     if nums2[n] == nums1[i]:
+        #         res.append(-1)
+        #         continue
+        #     while nums2[n] != nums1[i]:
+        #         if nums2[n] > nums1[i]:
+        #             mn = nums2[n]
+        #         n -= 1
+        #     if mn == float("inf"):
+        #         res.append(-1)
+        #     else:
+        #         res.append(mn)
+        # return res
